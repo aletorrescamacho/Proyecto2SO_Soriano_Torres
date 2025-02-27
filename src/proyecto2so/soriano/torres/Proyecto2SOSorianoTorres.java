960de53fb5a417cdca351d06d11d3e901bbe8e5c
aletorrescamacho <alessandra.torres@correo.unimet.edu.ve>
2025-02-27 11:16:29 -0400

    a master

---
+++ src/proyecto2so/soriano/torres/Proyecto2SOSorianoTorres.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto2so.soriano.torres;
+
+/**
+ *
+ * @author Aless
+ */
+public class Proyecto2SOSorianoTorres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
